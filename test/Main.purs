71e8f83f085f2b4b81371abbe5cfaf2913302dc5
Phil Freeman <paf31@cantab.net>
2016-08-24 21:32:35 -0700

    first commit

---
+++ test/Main.purs
@@ -0,0 +1,28 @@
+module Test.Main where
+
+import Prelude
+import Control.Lazy (fix)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log, logShow)
+import Data.Array (fromFoldable)
+import Data.List.Lazy (take)
+import Data.Rational (fromInt)
+import Data.Taylor (coefficients, integrate, constant, d, x)
+
+main :: forall e. Eff (console :: CONSOLE | e) Unit
+main = do
+  let series = constant (fromInt 1) + x * x
+
+  log "d(1 + x^2)/dx ="
+  logShow (fromFoldable (take 5 (coefficients (d series))))
+
+  log "∫ (1 + x^2) dx ="
+  logShow (fromFoldable (take 5 (coefficients (integrate series))))
+
+  let exp = fix \e -> constant (fromInt 1) + integrate e
+
+  log "1 / (1 + x^2) ="
+  logShow (fromFoldable (take 5 (coefficients (one / series))))
+
+  log "∫ dx / (1 + x^2) ="
+  logShow (fromFoldable (take 5 (coefficients (integrate (one / series)))))
